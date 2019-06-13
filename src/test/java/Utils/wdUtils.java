e8f0fdd83126ba20183c298a5968950a130ec5e4
axz-abstract <51722803+axz-abstract@users.noreply.github.com>
2019-06-13 15:32:52 -0300

    Update wdUtils.java

--- src/test/java/Utils/wdUtils.java
+++ src/test/java/Utils/wdUtils.java
@@ -32,6 +32,10 @@ public class wdUtils {
 	}
 	
 	public wdUtils() {
+		System.setProperty("base_dir",System.getProperty("user.dir").replace("target", "")); //al ejecutar desde maven la ruta del proyecto se modifica
+		System.out.println(System.getProperty("user.dir"));
+		System.out.println(System.getProperty("base_dir"));
+		
 		InputStream in = getClass().getResourceAsStream("config.properties");
 		prop = new Properties();
 		try {
