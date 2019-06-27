c52df858d3631ad6cff08d7c11f214933275b8a3
axz-abstract <51722803+axz-abstract@users.noreply.github.com>
2019-06-27 16:53:39 -0300

    Update BaseTest.java

--- src/test/java/Tests/BaseTest.java
+++ src/test/java/Tests/BaseTest.java
@@ -38,6 +38,7 @@ public abstract class BaseTest {
 		System.setProperty("base_dir",System.getProperty("user.dir").replace("target", "")); //al ejecutar desde maven la ruta del proyecto se modifica
 		System.setProperty("webdriver.chrome.driver",Constants.CHROME_DRIVER);
 		System.setProperty("webdriver.gecko.driver",Constants.FIREFOX_DRIVER);
+		System.out.println(System.getProperty("os.name"));
 		//driverf = new FirefoxDriver();
 		driverc = instanceofchrome();
 		driverc.get(wd.getProperty("BASE_URL"));
